4f8ded070f15b3472885afebc2462bc546c3c2f2
Jonas Stumph Stevnsvig <jonas.stumph@stevnsvig.com>
2022-12-09 20:51:45 +0100

    adding tests

--- tests/unit/DummyTest.php
+++ tests/unit/DummyTest.php
@@ -20,4 +20,24 @@ class DummyTest extends TestCase
         $this->assertTrue($dc instanceof dummyclass);
         $this->assertEquals($value, $dc->getValue());
     }
+
+    public function testDummyToo()
+    {
+        $values= ['extra', 'something', 'I', 'cannot', 'think','eggstra'];
+        $counter = 0;
+        foreach ($values as $val)
+        {
+          $dc = new dummyclass($val);
+          $this->assertTrue($dc instanceof dummyclass);
+          $this->assertEquals($val, $dc->getValue());
+        }
+    }
+
+    public function testDummyNumber()
+    {
+        $value = 12673;
+        $dc = new dummyclass($value);
+        $this->assertTrue($dc instanceof dummyclass);
+        $this->assertEquals($value, $dc->getValue());
+    }
 }
