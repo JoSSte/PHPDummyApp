353cbbd1269bca2173f88cd47476c93577879e25
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2022-12-09 22:26:54 +0100

    increased code coverage

--- tests/unit/DummyTest.php
+++ tests/unit/DummyTest.php
@@ -10,74 +10,82 @@ use dummy\dummyclass;
  */
 class DummyTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function testDummyBasic()
-    {
-        $value = 'comment';
-        $dc = new dummyclass($value);
-        $this->assertTrue($dc instanceof dummyclass);
-        $this->assertEquals($value, $dc->getValue(), 'Testing simple string');
-    }
+  /**
+   * @test
+   */
+  public function testDummyBasic(): void
+  {
+    $value = 'comment';
+    $dc = new dummyclass($value);
+    $this->assertTrue($dc instanceof dummyclass);
+    $this->assertEquals($value, $dc->getValue(), 'Testing simple string');
+  }
 
-    /**
-      * testing without paramaters... = more assertions, less testcases (assertions do not count in junit test report)
-      */
-    public function testDummyToo()
-    {
-        $values= ['extra', 'something', 'I', 'cannot', 'think','eggstra'];
-        $counter = 0;
-        foreach ($values as $val)
-        {
-          $dc = new dummyclass($val);
-          $this->assertTrue($dc instanceof dummyclass);
-          $this->assertEquals($val, $dc->getValue());
-        }
+  /**
+   * testing without paramaters... = more assertions, less testcases (assertions do not count in junit test report)
+   */
+  public function testDummyToo(): void
+  {
+    $values = ['extra', 'something', 'I', 'cannot', 'think', 'eggstra'];
+    $counter = 0;
+    foreach ($values as $val) {
+      $dc = new dummyclass($val);
+      $this->assertTrue($dc instanceof dummyclass);
+      $this->assertEquals($val, $dc->getValue());
     }
+  }
 
-    public function testDummyNumber()
-    {
-        $value = 12673;
-        $dc = new dummyclass($value);
-        $this->assertTrue($dc instanceof dummyclass);
-        $this->assertEquals($value, $dc->getValue());
+  public function testDummyNumber(): void
+  {
+    $value = 12673;
+    $dc = new dummyclass($value);
+    $this->assertTrue($dc instanceof dummyclass);
+    $this->assertEquals($value, $dc->getValue());
+  }
+
+  public function testDummyNumberSet(): void
+  {
+    $value = 12673;
+    $valuetoo = 'hello';
+    $dc = new dummyclass($value);
+    $this->assertTrue($dc instanceof dummyclass);
+    $this->assertEquals($value, $dc->getValue());
+    $dc->setValue($valuetoo);
+    $this->assertEquals($valuetoo, $dc->getValue());
+
+  }
+
+
+  /**
+   * @dataProvider providerStrings
+   */
+  public function testDummyParameterized(string|int|bool $value, string $explain, string $expectedResult = '¤', bool $expectEquality = true)
+  {
+    if ($expectedResult == '¤') {
+      $expectedResult = $value;
     }
-
-
-    /**
-      * @dataProvider providerStrings
-      */
-    public function testDummyParameterized(
-           $value,
-           string $explain,
-           string $expectedResult = '¤',
-           bool $expectEquality = true)
-    {
-        if($expectedResult == '¤'){
-           $expectedResult = $value;
-        }
-        $dc = new dummyclass($value);
-        $this->assertTrue($dc instanceof dummyclass);
-        if($expectEquality){
-          $this->assertEquals($value, $dc->getValue(), $explain);
-        }else{
-          $this->assertNotEquals($expectedResult, $dc->getValue(), $explain);
-        }
+    $dc = new dummyclass($value);
+    $this->assertTrue($dc instanceof dummyclass);
+    if ($expectEquality) {
+      $this->assertEquals($value, $dc->getValue(), $explain);
+    } else {
+      $this->assertNotEquals($expectedResult, $dc->getValue(), $explain);
     }
+  }
 
-    /**
-      * @dataProvider providerStrings
-      */
-    public function providerStrings() {
-      return array(
-          array('extra', 'checking string'),
-          array('something', 'checking string'),
-          array('I', 'checking string'),
-          array('cannot', 'checking string'),
-          array('think', 'checking string'),
-          array(false, 'checking boolean', ''),
-          array(0, 'checking number')
-         );
-    }
+  /**
+   * @dataProvider providerStrings
+   */
+  public function providerStrings()
+  {
+    return array(
+      array('extra', 'checking string'),
+      array('something', 'checking string'),
+      array('I', 'checking string'),
+      array('cannot', 'checking string'),
+      array('think', 'checking string'),
+      array(false, 'checking boolean', ''),
+      array(0, 'checking number')
+    );
+  }
 }
